9890ce9c36b19307fbec2274740cc12d7bac90c9
843905 <843905@01HW290135.India.TCS.com>
2017-10-11 16:58:29 +0530

    ok

--- service-provider-api/src/main/java/com/p2m/serviceprovider/security/WebSecurityConfig.java
+++ service-provider-api/src/main/java/com/p2m/serviceprovider/security/WebSecurityConfig.java
@@ -29,7 +29,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // Create a default account
+        // Create a default account    	
         auth.inMemoryAuthentication()
             .withUser("admin")
             .password("password")
