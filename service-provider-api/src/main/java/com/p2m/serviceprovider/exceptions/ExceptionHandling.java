a374e6b49ba4af2a964bd30e28e2e512dac15ff3
843905 <843905@01HW290135.India.TCS.com>
2017-10-16 16:02:26 +0530

    ok

--- service-provider-api/src/main/java/com/p2m/serviceprovider/exceptions/ExceptionHandling.java
+++ service-provider-api/src/main/java/com/p2m/serviceprovider/exceptions/ExceptionHandling.java
@@ -1,5 +1,28 @@
 package com.p2m.serviceprovider.exceptions;
 
-public class ExceptionHandling {
+public class ExceptionHandling extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
+	public ExceptionHandling() {
+		super();
+	}
+
+	public ExceptionHandling(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public ExceptionHandling(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public ExceptionHandling(String message) {
+		super(message);
+	}
+
+	public ExceptionHandling(Throwable cause) {
+		super(cause);
+	}
 }
